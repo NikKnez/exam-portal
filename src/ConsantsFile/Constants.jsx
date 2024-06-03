f3cf55bec2fd93f7cb768ac9b6f84712732e54db
NikKnez <nknezevic07@gmail.com>
2024-06-03 11:19:54 +0200

    modified:   src/ConsantsFile/Constants.jsx

--- src/ConsantsFile/Constants.jsx
+++ src/ConsantsFile/Constants.jsx
@@ -1,6 +1,7 @@
 const prod = {
 	url: {
-		BASE_URL: 'http://localhost:8080/api',
+		// BASE_URL: 'http://localhost:8080/api',
+		BASE_URL: 'https://exam-portal-ef445cdfbd84.herokuapp.com/api',
 	},
 };
 
